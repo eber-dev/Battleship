06f4707713beefbc4c739380cd9edb47824a7e6c
Eber <www.eberacunaquispe@gmail.com>
2026-05-27 00:05:46 -0500

    Se añadio validacion en placeShip asimismo se añadio verificacion de las casillas atacadas y un retorno adecuando en receiveattack asimismo ahora se borra el array qeu regstra las casillas atacadas

--- src/classes/Gameboard.js
+++ src/classes/Gameboard.js
@@ -9,8 +9,7 @@ export class Gameboard {
     placeShip(size, x, y, direction) {
         const ship = new Ship(size);
 
-        if (x > 9 || y > 9) {
-            alert('Coordenadas invalidas');
+        if (x > 9 || y > 9 || x < 0 || y < 0) {
             return;
         }
 
@@ -42,23 +41,30 @@ export class Gameboard {
     }
 
     receiveAttack(x, y) {
+        if (x > 9 || y > 9 || x < 0 || y < 0) {
+            return;
+        }
+
         const target = this.board[x][y];
+        let atacado = this.attacked.some((coordenada) => {
+            return coordenada[0] === x && coordenada[1] === y;
+        });
+
+        if (atacado === true) {
+            return;
+        }
 
         if (target === null) {
             this.attacked.push([x, y]);
-            alert('Miss');
-            this.attacked.forEach((subarray) => {
-                if (subarray[0] === x && subarray[1] === y) {
-                    return;
-                }
-            });
         } else {
+            this.attacked.push([x, y]);
             target.hit();
         }
     }
 
     clean() {
         this.board = Array.from({ length: 10 }, () => Array(10).fill(null));
+        this.attacked.splice(0, this.attacked.length);
     }
 
     allshipSunks() {
@@ -67,11 +73,10 @@ export class Gameboard {
                 if (this.board[i][j] !== null) {
                     if (!this.board[i][j].isSunk()) {
                         return false;
-                    } else {
-                        return true;
                     }
                 }
             }
         }
+        return true;
     }
 }
